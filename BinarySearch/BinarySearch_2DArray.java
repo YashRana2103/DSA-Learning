1053c5de96df37d6c44f4d1f41f53531d6ab5927
Yash <yashrana21032006@gmail.com>
2024-05-19 09:30:47 +0530

    correction in last commit

--- BinarySearch/BinarySearch_2DArray.java
+++ BinarySearch/BinarySearch_2DArray.java
@@ -8,16 +8,18 @@ public class BinarySearch_2DArray {
                 {5, 10, 15, 20},
                 {11, 16, 21, 26},
                 {21, 25, 30, 35},
-                {27, 32, 37, 42}
+                {22, 27, 31, 36},
+                {37, 38, 39, 40}
         };
 
-        System.out.println(Arrays.toString(search(arr, 4)));
+        System.out.println(Arrays.toString(search(arr, 38)));
     }
 
     static int[] search(int[][] matrix, int target){
 //        work only when row and col both are sorted
         int row = 0;
-        int col = matrix.length - 1;
+//        int col = matrix.length - 1; //In case of where row=col (n*n)
+        int col = matrix[row].length - 1; //In case of where row!=col (n*m)
 
         while(row < matrix.length && col>=0){
             if(matrix[row][col]==target){
