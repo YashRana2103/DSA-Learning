5fcf8656173b46c1ed771921d60cf5ad98424b95
Yash <yashrana21032006@gmail.com>
2024-05-25 07:18:41 +0530

    Recursion_fibonacci

---
+++ Recursion/Fibo.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Fibo {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(fibo(i)+" ");
+        }
+    }
+
+    static int fibo(int n){
+        if (n < 2){
+            if (n == 1){
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        return fibo (n - 1) + fibo (n - 2);
+    }
+}
