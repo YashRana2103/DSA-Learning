339964d5114d121fc2c10fa302fa2868a0a96d34
Yash <yashrana21032006@gmail.com>
2024-06-06 10:30:18 +0530

    .

--- KK/Recursion/Practice.java
+++ KK/Recursion/Practice.java
@@ -78,8 +78,8 @@ public class Practice {
 
     private static int fibo(int n) {
         int[] arr = new int[n];
-        if(n<=2){
-            if(n==0 || n==1) return 0;
+        if(n<=1){
+            if(n==0) return 0;
             return 1;
         }
 
