cb03f7a3d43a1a5f4162a470e843f39a1bd8c261
Yash <yashrana21032006@gmail.com>
2024-06-06 11:15:10 +0530

    .

--- src/TUF/Hashing.java
+++ src/TUF/Hashing.java
@@ -1,5 +1,52 @@
 package TUF;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Hashing {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter size of the array: ");
+        int sizeOfArray = input.nextInt();
+
+        int[] arr = new int[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++) {
+            System.out.print("Enter arr["+i+"]: ");
+            arr[i] = input.nextInt();
+        }
+
+        int[] hashArray = createHashArray(arr, sizeOfArray);
+
+        System.out.println();
+
+        System.out.print("How many times do you want to check: ");
+        int checkTime = input.nextInt();
+
+        for (int i = 0; i < checkTime; i++) {
+            System.out.println();
+            System.out.println("check "+(i+1)+": ");
+            System.out.print("Enter number to check: ");
+            int number = input.nextInt();
+            System.out.println(number+" appears "+checkNumbers(hashArray, number, sizeOfArray)+" times in the data");
+            System.out.print("-------------------------------------");
+        }
+    }
     
+    private static int checkNumbers(int[] hashArr, int number, int sizeOfArray) {
+        return hashArr[number];
+    }
+
+    private static int[] createHashArray(int[] arr, int sizeOfArray) {
+        int[] hashArr = new int[sizeOfArray*2];
+
+        for (int i = 0; i < hashArr.length; i++) {
+            hashArr[i] = 0;
+        }
+
+        for (int i = 0; i < sizeOfArray; i++) {
+            hashArr[arr[i]]++;
+        }
+        return hashArr;
+    }
 }
