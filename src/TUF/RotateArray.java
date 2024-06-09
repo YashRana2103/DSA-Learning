9e92962bebd3b67357dfa35ea43116cf5cd0e80d
Yash <yashrana21032006@gmail.com>
2024-06-09 09:27:16 +0530

    .

---
+++ src/TUF/RotateArray.java
@@ -0,0 +1,36 @@
+package TUF;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+//        System.out.println(Arrays.toString(rotateLeft(arr)));
+        System.out.println(Arrays.toString(rotateLeftNtimes(arr, 3)));
+    }
+
+    private static int[] rotateLeft(int[] arr) {
+        int temp = arr[0];
+
+        for (int i = 1; i <= arr.length - 1; i++) {
+            arr[i - 1] = arr[i];
+        }
+
+        arr[arr.length - 1] = temp;
+        return arr;
+    }
+
+    private static int[] rotateLeftNtimes(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            int temp = arr[0];
+
+            for (int j = 1; j <= arr.length - 1; j++) {
+                arr[j - 1] = arr[j];
+            }
+
+            arr[arr.length - 1] = temp;
+        }
+        return arr;
+    }
+}
