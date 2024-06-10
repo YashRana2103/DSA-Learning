f408bda4ceba600a13aeb19d8e2ed8379d8c14ac
Yash <yashrana21032006@gmail.com>
2024-06-10 10:09:04 +0530

    .

--- src/TUF/MissingNumber.java
+++ src/TUF/MissingNumber.java
@@ -2,12 +2,17 @@ package TUF;
 
 public class MissingNumber {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {2, 3, 4, 5, 6};
 
         System.out.println(missingNumber(arr, 6));
     }
 
     private static int missingNumber(int[] arr, int n) {
+//        return betterSol(arr, n);
+        return optimalSol(arr, n);
+    }
+
+    private static int betterSol(int[] arr, int n){
         int[] hashArr = new int[n+1];
 
         for (int i = 0; i < arr.length; i++) {
@@ -19,7 +24,18 @@ public class MissingNumber {
                 return i;
             }
         }
-
         return -1;
     }
-}
+
+    private static int optimalSol(int[] arr, int n) {
+        int calculatedSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            calculatedSum += arr[i];
+        }
+
+        return ((n * (n + 1)) / 2) - calculatedSum;
+
+//        ((n * (n + 1)) / 2) is the actual sum
+    }
+ }
