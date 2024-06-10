54ad0502401589b8b5c98a0d334e03bf25dcb472
Yash <yashrana21032006@gmail.com>
2024-06-10 10:22:33 +0530

    .

---
+++ src/TUF/MaxConsecutiveOne.java
@@ -0,0 +1,26 @@
+package TUF;
+
+import static java.lang.Math.max;
+
+public class MaxConsecutiveOne {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1};
+
+        System.out.println(maxConsecutiveOne(arr));
+    }
+
+    private static int maxConsecutiveOne(int[] arr) {
+        int count = 0;
+        int max = count;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == 1) {
+                count++;
+                max = max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return max;
+    }
+}
