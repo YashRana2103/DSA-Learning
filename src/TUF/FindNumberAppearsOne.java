85847a35d8d17ea4d613dbe46bfbade3c36d2fac
Yash <yashrana21032006@gmail.com>
2024-06-10 10:48:20 +0530

    .

--- src/TUF/FindNumberAppearsOne.java
+++ src/TUF/FindNumberAppearsOne.java
@@ -10,6 +10,11 @@ public class FindNumberAppearsOne {
     }
     
     private static int numberAppearsOne(int[] arr, int n) {
+//        return betterSol(arr, n);
+        return optimalSol(arr, n);
+    }
+
+    private static int betterSol(int[] arr, int n) {
         int max = arr[0];
         for (int i = 1; i < n; i++) {
             max = max(arr[i], max);
@@ -28,7 +33,15 @@ public class FindNumberAppearsOne {
                 return i;
             }
         }
-        
+
         return -1;
+    }
+
+    private static int optimalSol(int[] arr, int n) {
+        int xor = 0;
+        for (int i = 0; i < n; i++) {
+            xor ^= arr[i];
+        }
+        return xor;
     }
 }
