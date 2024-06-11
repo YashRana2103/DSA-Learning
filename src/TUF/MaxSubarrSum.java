8cc293c9a336661d3730a2aa8422ce23149abbcb
Yash <yashrana21032006@gmail.com>
2024-06-11 17:19:34 +0530

    .

--- src/TUF/MaxSubarrSum.java
+++ src/TUF/MaxSubarrSum.java
@@ -7,6 +7,7 @@ public class MaxSubarrSum {
         int[] arr = {-1, -2, 1, 3, -2, 4, -5, 6};
 
         System.out.println(kadaneAlgo(arr));
+        kadaneAlgo_printSubArr(arr, arr.length - 1);
     }
 
     private static int kadaneAlgo(int[] arr) {
@@ -25,5 +26,30 @@ public class MaxSubarrSum {
         }
 
         return max;
+    }
+
+    private static void kadaneAlgo_printSubArr(int[] arr, int n) {
+        // not working
+        int sum = 0;
+        int max = arr[0];
+        int start = 0;
+        int ansStart = -1;
+        int ansEnd = -1;
+
+        for(int i = 0; i < n; i++) {
+            if(sum == 0) start = i;
+            sum += arr[i];
+            if(sum > max) {
+                max = sum;
+                ansStart = start;
+                ansEnd = i;
+            }
+
+            if(sum < 0) sum = 0;
+        }
+
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i]+" ");
+        }
     }
 }
