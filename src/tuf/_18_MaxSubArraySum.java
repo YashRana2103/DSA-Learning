e120c221436438308d0e802fdd133eafd8191f09
Yash <yashrana21032006@gmail.com>
2024-06-19 10:45:46 +0530

    .

--- src/tuf/_18_MaxSubArraySum.java
+++ src/tuf/_18_MaxSubArraySum.java
@@ -4,7 +4,8 @@ import static java.lang.Math.max;
 
 public class _18_MaxSubArraySum {
     public static void main(String[] args) {
-        int[] arr = {-1, -2, 1, 3, -2, 4, -5, 6};
+//        int[] arr = {-1, -2, 1, 3, -2, 4, -5, 6};
+        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
 
         System.out.println(kadaneAlgo(arr));
         kadaneAlgo_printSubArr(arr, arr.length - 1);
