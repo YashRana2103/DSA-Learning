017bf7c8e943d3f46acf4ef12fa23dde710443a6
Yash <yashrana21032006@gmail.com>
2024-06-19 10:51:43 +0530

    .

--- src/selfpractice/_16_MaxSubArraySum.java
+++ src/selfpractice/_16_MaxSubArraySum.java
@@ -6,7 +6,6 @@ public class _16_MaxSubArraySum {
     public static void main(String[] args) {
         int[] arr = {-1, -2, 1, 3, -2, 4, -5, 6};
         System.out.println(kadaneAlgo(arr, arr.length));
-        kadaneAlgo_printSubArr(arr, arr.length - 1);
     }
     public static int kadaneAlgo(int[] arr, int n) {
         int sum = 0;
