c76d59925874cfa12f21d0638d621cb69455eac7
Yash <yashrana21032006@gmail.com>
2024-06-22 09:35:34 +0530

    .

--- src/tuf/_22_BS_LowerBound.java
+++ src/tuf/_22_BS_LowerBound.java
@@ -4,6 +4,7 @@ public class _22_BS_LowerBound {
     public static void main(String[] args) {
         int[] arr = {3, 4, 4, 5, 7, 8, 9, 10, 11, 15, 21};
         System.out.println(lowerBound(arr, arr.length, 10));
+        System.out.println(upperBound(arr, arr.length, 4));
     }
     public static int lowerBound(int[] arr, int n, int target) {
         int ans = n;
@@ -11,6 +12,20 @@ public class _22_BS_LowerBound {
         while(low <= high) {
             int mid = low + (high - low) / 2;
             if(arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static int upperBound(int[] arr, int n, int target) {
+        int ans = n;
+        int low = 0, high = n - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(arr[mid] > target) {
                 ans = mid;
                 high = mid - 1;
             } else {
