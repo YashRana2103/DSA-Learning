01e3756a4ad76620422bbc7817ccc29006ccd488
Yash <yashrana21032006@gmail.com>
2024-06-25 10:13:45 +0530

    .

--- src/selfpractice/_23_BS_FirstLastOccurance.java
+++ src/selfpractice/_23_BS_FirstLastOccurance.java
@@ -8,7 +8,7 @@ import static tuf._22_BS_LowerBound_UpperBound.*;
 public class _23_BS_FirstLastOccurance {
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 7, 7, 7, 7, 9, 10};
-//        System.out.println(Arrays.toString(firstLastOccurance_LB_UB(arr, arr.length, 7)));
+        System.out.println(Arrays.toString(firstLastOccurance_LB_UB(arr, arr.length, 7)));
         System.out.println(Arrays.toString(firstLastOccurance_BS(arr, arr.length, 7)));
     }
     public static int[] firstLastOccurance_LB_UB(int[] arr, int n, int value) {
