e97c932ce1c84397265c5106695e2b6e34081be9
Yash <yashrana21032006@gmail.com>
2024-06-27 10:12:29 +0530

    .

---
+++ src/selfpractice/_26_BS_II_SearchRotatedArray.java
@@ -0,0 +1,28 @@
+package selfpractice;
+
+public class _26_BS_II_SearchRotatedArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 3, 3, 4, 4, 5, 1, 2, 3, 3, 3};
+        System.out.println(searchRotatedArray(arr, arr.length, 1));
+    }
+    public static boolean searchRotatedArray(int[] arr, int n, int target) {
+        int low = 0, high = n - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == target) return true;
+            if(arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low++;
+                high--;
+                continue;
+            }
+            if(arr[low] <= arr[mid]) {
+                if(arr[low] <= target && target <= arr[mid]) high = mid - 1;
+                else low = mid + 1;
+            } else {
+                if(arr[mid] <= target && target <= arr[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
