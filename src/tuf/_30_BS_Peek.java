2e14c739f259ae4abc1fce845abc58ff7d68a01d
Yash <yashrana21032006@gmail.com>
2024-06-29 09:49:55 +0530

    .

---
+++ src/tuf/_30_BS_Peek.java
@@ -0,0 +1,21 @@
+package tuf;
+
+public class _30_BS_Peek {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6, 9, 10, 8, 7, 4, 1, 0};
+        System.out.println(peek(arr, arr.length));
+    }
+    public static int peek(int[] arr, int n) {
+        if(n == 1) return arr[0];
+        if(arr[0] > arr[1]) return arr[0];
+        if(arr[n - 1] > arr[n - 2]) return arr[n - 1];
+        int l = 1, h = n - 2;
+        while(l <= h) {
+            int m = l + (h - l) / 2;
+            if(arr[m] > arr[m - 1] && arr[m] > arr[m + 1]) return arr[m];
+            else if(arr[m] > arr[m - 1]) l = m + 1;
+            else h = m - 1;
+        }
+        return -1;
+    }
+}
