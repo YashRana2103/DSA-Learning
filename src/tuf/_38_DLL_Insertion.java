ad5fccdce4db9e4e0cec0b3c4962e4d3cd9459c7
Yash <yashrana21032006@gmail.com>
2024-06-30 11:47:18 +0530

    .

--- src/tuf/_38_DLL_Insertion.java
+++ src/tuf/_38_DLL_Insertion.java
@@ -47,7 +47,8 @@ public class _38_DLL_Insertion {
         int[] arr = {3, 1, 4, 8, 9, 2, 10};
         Node head = convertArrToDLL(arr);
 
-        head = insertAtHead(head, 134);
+//        head = insertAtHead(head, 134);
+        head = insertAtTail(head, 134);
         print(head);
     }
 
@@ -56,6 +57,14 @@ public class _38_DLL_Insertion {
         Node newNode = new Node(null, num, head);
         head.prev = newNode;
         head = newNode;
+        return head;
+    }
+
+    public static Node insertAtTail(Node head, int num) {
+        if(head == null) return new Node(num);
+        Node temp = head;
+        while(temp.next != null) temp = temp.next;
+        temp.next = new Node(temp, num, null);
         return head;
     }
 }
