02bb15dcf482da5a9e174d35788382650ecb14da
Yash <yashrana21032006@gmail.com>
2024-07-02 19:40:14 +0530

    .

--- src/selfpractice/_34_DLL_Intro.java
+++ src/selfpractice/_34_DLL_Intro.java
@@ -23,7 +23,26 @@ public class _34_DLL_Intro {
         }
     }
 
+    public static Node convertArrToDLL(int[] arr, int n) {
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for (int i = 1; i < n; i++) {
+            mover.next = new Node(mover, arr[i]);
+            mover = mover.next;
+        }
+        return head;
+    }
+
+    public static void print(Node head) {
+        while(head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {3, 5, 6, 9, 10, 14, 20};
+        Node head = convertArrToDLL(arr, arr.length);
+        print(head);
     }
 }
